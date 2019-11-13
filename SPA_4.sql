9b60ef597e547cd88ede195dd3e8dd9364e09e73
Franck-chef <57718266+Franck-chef@users.noreply.github.com>
2019-11-13 17:39:33 +0100

    Add files via upload

---
+++ SPA_4.sql
@@ -0,0 +1,158 @@
+/*==============================================================*/
+/* Nom de SGBD :  MySQL 5.0                                     */
+/* Date de création :  13/11/2019 17:37:56                      */
+/*==============================================================*/
+
+
+drop table if exists ANCIEN;
+
+drop table if exists ANIMAL;
+
+drop table if exists ESPECE;
+
+drop table if exists IDENTIFICATION;
+
+drop table if exists NOUVEAU;
+
+drop table if exists PROPRIETAIRE;
+
+drop table if exists RACE;
+
+drop table if exists STATUT;
+
+drop table if exists UTILISATEUR;
+
+/*==============================================================*/
+/* Table : ANCIEN                                               */
+/*==============================================================*/
+create table ANCIEN
+(
+   IDANIMAL             int not null,
+   IDPRO                int not null,
+   primary key (IDANIMAL, IDPRO)
+);
+
+/*==============================================================*/
+/* Table : ANIMAL                                               */
+/*==============================================================*/
+create table ANIMAL
+(
+   IDANIMAL             int not null,
+   CODEIDEN             varchar(2),
+   CODESTA              varchar(2) not null,
+   CODERACE             varchar(4) not null,
+   NOMANIMAL            varchar(30),
+   DATEARRIVEANIMAL     date,
+   DATEDEPARTANIMAL     date,
+   DESCRIPTION          varchar(200),
+   SEXE                 bool,
+   CASTRE               bool,
+   AGE                  int,
+   VACCINER             bool,
+   IMAGE                varchar(40),
+   PRIXADOPTION         numeric(6,2),
+   primary key (IDANIMAL)
+);
+
+/*==============================================================*/
+/* Table : ESPECE                                               */
+/*==============================================================*/
+create table ESPECE
+(
+   CODEES               varchar(3) not null,
+   LIBELLEES            varchar(30),
+   primary key (CODEES)
+);
+
+/*==============================================================*/
+/* Table : IDENTIFICATION                                       */
+/*==============================================================*/
+create table IDENTIFICATION
+(
+   CODEIDEN             varchar(2) not null,
+   LIBELLEIDEN          varchar(30),
+   primary key (CODEIDEN)
+);
+
+/*==============================================================*/
+/* Table : NOUVEAU                                              */
+/*==============================================================*/
+create table NOUVEAU
+(
+   IDANIMAL             int not null,
+   IDPRO                int not null,
+   primary key (IDANIMAL, IDPRO)
+);
+
+/*==============================================================*/
+/* Table : PROPRIETAIRE                                         */
+/*==============================================================*/
+create table PROPRIETAIRE
+(
+   IDPRO                int not null,
+   NOMPRO               longtext,
+   PRENOMPRO            varchar(40),
+   MAILPRO              varchar(40),
+   ADRESSEPRO           varchar(40),
+   primary key (IDPRO)
+);
+
+/*==============================================================*/
+/* Table : RACE                                                 */
+/*==============================================================*/
+create table RACE
+(
+   CODERACE             varchar(4) not null,
+   CODEES               varchar(3) not null,
+   LIBELLERACE          varchar(40),
+   primary key (CODERACE)
+);
+
+/*==============================================================*/
+/* Table : STATUT                                               */
+/*==============================================================*/
+create table STATUT
+(
+   CODESTA              varchar(2) not null,
+   LIBELLESTA           varchar(30),
+   primary key (CODESTA)
+);
+
+/*==============================================================*/
+/* Table : UTILISATEUR                                          */
+/*==============================================================*/
+create table UTILISATEUR
+(
+   IDUSER               int not null,
+   NOMUSER              varchar(30),
+   PRENOMUSER           varchar(30),
+   ADMINUSER            bool,
+   IDENTIFIANTUSER      varchar(30),
+   MDPUSER              varchar(30),
+   primary key (IDUSER)
+);
+
+alter table ANCIEN add constraint FK_ANCIEN foreign key (IDANIMAL)
+      references ANIMAL (IDANIMAL) on delete restrict on update restrict;
+
+alter table ANCIEN add constraint FK_ANCIEN2 foreign key (IDPRO)
+      references PROPRIETAIRE (IDPRO) on delete restrict on update restrict;
+
+alter table ANIMAL add constraint FK_APPARTENIR foreign key (CODERACE)
+      references RACE (CODERACE) on delete restrict on update restrict;
+
+alter table ANIMAL add constraint FK_ETRE foreign key (CODESTA)
+      references STATUT (CODESTA) on delete restrict on update restrict;
+
+alter table ANIMAL add constraint FK_IDENTIFIER foreign key (CODEIDEN)
+      references IDENTIFICATION (CODEIDEN) on delete restrict on update restrict;
+
+alter table NOUVEAU add constraint FK_NOUVEAU foreign key (IDANIMAL)
+      references ANIMAL (IDANIMAL) on delete restrict on update restrict;
+
+alter table NOUVEAU add constraint FK_NOUVEAU2 foreign key (IDPRO)
+      references PROPRIETAIRE (IDPRO) on delete restrict on update restrict;
+
+alter table RACE add constraint FK_CORRESPONDRE foreign key (CODEES)
+      references ESPECE (CODEES) on delete restrict on update restrict;
+
